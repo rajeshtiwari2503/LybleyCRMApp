00490acc425454d068c54201ae319c8d7a6b40c0
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-10-19 14:58:32 +0530

    hhfcxsssccjhjhjhjh

--- app/AppNavigation.jsx
+++ app/AppNavigation.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import UserNavigator from './UserNavigation';
 import DealerNavigator from './DealerNavigation';
 import TechnicianNavigator from './TechnicianNavigation';
 import Login from "../components/Login";
-
+import { useFonts } from 'expo-font';
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   const [user, setUser] = useState(null);
- 
+
+  const [fontsLoaded] = useFonts({
+    'outfit': require('../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium': require('../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold': require('../assets/fonts/Outfit-Bold.ttf'),
+  });
+
   useEffect(() => {
     const checkUser = async () => {
       try {
@@ -22,16 +28,21 @@ const AppNavigator = () => {
         }
       } catch (error) {
         console.log('Error retrieving user data', error);
-      } finally {
-        // setLoading(false);
       }
     };
 
     checkUser();
   }, []);
 
- 
-
+  if (!fontsLoaded) {
+    console.log("Fonts not loaded yet.");
+    // You can return null or some basic component if you want something to render before fonts load
+    return null;
+  }
+  if (fontsLoaded) {
+    console.log("Fonts successfully loaded.");
+    
+  }
   return (
     <Stack.Navigator 
       screenOptions={{
